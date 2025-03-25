82e3e5c29ba50d56d0227a8839aa9ab009e9ebc8
Yehor Smoliakov <egorsmkv@gmail.com>
2025-03-25 11:24:08 +0200

    Sync

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,38 @@ fn image_to_yolo(original_image: &DynamicImage) -> Array4<f32> {
     input
 }
 
+fn image_to_yolo_two(original_image: &DynamicImage) -> Array4<f32> {
+    let mut input = ArrayBase::zeros((1, 640, 640, 3));
+
+    let image = original_image.resize_exact(640, 640, FilterType::CatmullRom);
+    for (x, y, Rgba([r, g, b, _])) in image.pixels() {
+        let x = x as usize;
+        let y = y as usize;
+
+        input[[0, y, x, 0]] = (r as f32) / 255.;
+        input[[0, y, x, 1]] = (g as f32) / 255.;
+        input[[0, y, x, 2]] = (b as f32) / 255.;
+    }
+
+    input
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let img = image::open("54c08a9-life-shura705.jpg")?;
     let yolo_im = image_to_yolo(&img);
 
+    let yolo_reshaped = yolo_im.clone().into_shape_with_order((1, 640, 640, 3)).unwrap();
+
     println!("Image: {:?}", yolo_im.shape());
+
+    println!("Image reshaped: {:?}", yolo_reshaped.shape());
+
+    let yolo_im2 = image_to_yolo_two(&img);
+
+    println!("Image 2: {:?}", yolo_im2.shape());
+
+    // println!("{:#?}", yolo_im);
+    // println!("{:#?}", yolo_im2);
 
     Ok(())
 }
